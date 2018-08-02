b2e47ba2861cb4e19ba554c4d9b4e74273243ae3
Dávid Lázár <dajo.lazar@gmail.com>
2018-08-02 16:56:42 +0200

    static template, md added, jekyll removed

---
+++ js/scrollto.js
@@ -0,0 +1,1 @@
+$(document).on("click",'a[href^="#"]',function(t){t.preventDefault(),$("html, body").animate({scrollTop:$($.attr(this,"href")).offset().top},700)});
---
+++ md/group-list.md
@@ -0,0 +1,8 @@
+vanda 
+	generalka 19.5
+	divadlo 20.5.
+	matura 21. - 24.5 + predstavenia 21., 22.
+	velka premiera 27.5. v divadle
+	28.5. - bratislava
+	
+	
