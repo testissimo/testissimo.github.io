9f22a1cd4c7ea473112998d80cd17d28b0ad3c1d
Matus Szabo <>
2018-10-02 17:28:39 +0200

    cleaned

--- start.js
+++ start.js
@@ -4,16 +4,8 @@
 // https://www.totaljs.com
 // ===================================================
 
-const options = {};
-
-// options.ip = '127.0.0.1';
-// options.port = parseInt(process.argv[2]);
- options.config = { 
-    "directory-public" : "./",
-    name: 'Total.js' 
-};
-// options.sleep = 3000;
-// options.inspector = 9229;
-// options.debugger = 40894;
-
-require('total.js/debug')(options);
+require('total.js/debug')({
+    config: {
+        'directory-public': './'
+    }
+});
