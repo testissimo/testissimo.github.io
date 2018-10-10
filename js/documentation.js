59ae1ab755e7e7122165b8a69929a1860f3cc485
Matus Szabo <matus.szabo@gmail.com>
2018-10-10 16:43:13 +0200

    refactored: css

--- js/documentation.js
+++ js/documentation.js
@@ -4,7 +4,7 @@ new Vue({
 		routes: [{ path: '/:id' }, { path: '*' }]
 	}),
 	data: {
-		sitemap: [],
+		sitemap: null,
 		page: {
 			index: -1,
 			id: '',
@@ -27,7 +27,7 @@ new Vue({
 			app.page.id = pageId;
 			app.page.index = app.sitemap.map(function(pageInfo){ return pageInfo.id; }).indexOf(pageId);
 			app.page.content = '';
-			app.page.title = app.page.index === -1 ? '' : app.sitemap[app.page.index].title;
+			app.page.title = app.page.index === -1 ? 'Documentation' : app.sitemap[app.page.index].title;
 
 			// scroll to top
 			window.scrollTo(0,0);
